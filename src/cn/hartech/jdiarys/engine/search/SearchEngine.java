0de27cb22b49cd34ee05e675d0cde8c3277bc753
Jeff.Z <jeff.zheng@qq.com>
2014-08-31 22:58:21 +0800

    #17 搜索引擎：增加多关键字"或"搜索，如“令狐冲 或 笑傲江湖"

--- src/cn/hartech/jdiarys/engine/search/SearchEngine.java
+++ src/cn/hartech/jdiarys/engine/search/SearchEngine.java
@@ -8,7 +8,7 @@ import cn.hartech.jdiarys.All;
 import cn.hartech.jdiarys.pojo.DiaryPOJO;
 
 /**
- * 本APP的搜索入口
+ * 日记搜索引擎
  * 
  * @author Jeff.Z
  * @date 2014年8月30日
@@ -18,15 +18,20 @@ public class SearchEngine {
 	/**
 	 * 这里是统一入口，主要是做分发
 	 * 
-	 * 1, 支持多关键字搜索：
-	 * 		如 "巴菲特 经济"
+	 * 下面同时也是判断顺序：
+	 * 
+	 * 1, 支持SQL搜索模式，可以输入SQL中的where子句，如:
+	 * 		"s content='abc' and hour=4"
+	 * 		"s length(content)>100"
 	 * 
 	 * 2, 日期搜索模式：
 	 * 		输入 "20120506" 或  "201205" 或  "2012" 返回该时间段的日记
 	 * 
-	 * 3, 支持SQL搜索模式，可以输入SQL中的where子句，如:
-	 * 		"s content='abc' and hour=4"
-	 * 		"s length(content)>100"
+	 * 3, 支持多关键字或集搜索：
+	 * 		如 "令狐冲 或  笑傲江湖"
+	 * 
+	 * 4, 支持多关键字并集搜索：
+	 * 		如 "巴菲特 经济"
 	 * 
 	 * @author Jeff
 	 * @date 2014年8月30日
@@ -53,12 +58,56 @@ public class SearchEngine {
 			return searchWithDate(text);
 		}
 
-		// 多关键字模式
-		else {
+		// 多关键字"或"模式
+		else if (text.contains(" 或 ") || text.contains(" or ")) {
 
-			return searchWithMutiWords(text);
+			return searchWithMutiORWords(text);
 		}
 
+		// 多关键字"并"模式
+		else {
+
+			return searchWithMutiANDWords(text);
+		}
+
+	}
+
+	/**
+	 * 支持多关键字或集搜索：
+	 * 		如 "令狐冲 或  笑傲江湖"
+	 * 
+	 * @author Jeff.Z
+	 * @date 2014年8月31日
+	 * @param text
+	 * @return
+	 */
+	private static List<DiaryPOJO> searchWithMutiORWords(String words) {
+
+		words = words.replace(" 或 ", " ");
+		words = words.replace(" or ", " ");
+
+		// 取出用空格隔开的多个关键字
+		String[] wordsArray = StringUtils.split(words);
+
+		String condition = "";
+
+		for (int i = 0; i < wordsArray.length; i++) {
+
+			if (i == 0) {
+				condition = " content like ? ";
+			} else {
+				condition += " or content like ? ";
+			}
+
+			wordsArray[i] = "%" + wordsArray[i] + "%";
+		}
+
+		String sql = "select * from diary_content where ( " + condition
+				+ " ) and is_delete = 0 order by diary_time desc";
+
+		List<DiaryPOJO> result = All.diaryDAO.getListWithSQL(sql, wordsArray);
+
+		return result;
 	}
 
 	/**
@@ -145,7 +194,7 @@ public class SearchEngine {
 	 * @param words
 	 * @return
 	 */
-	private static List<DiaryPOJO> searchWithMutiWords(String words) {
+	private static List<DiaryPOJO> searchWithMutiANDWords(String words) {
 
 		// 取出用空格隔开的多个关键字
 		String[] wordsArray = StringUtils.split(words);
